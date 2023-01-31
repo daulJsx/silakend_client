1f6894d7c73fed658053d7266247ae0ebdc62569
daulJsx <mrdaul09@gmail.com>
2023-01-31 10:49:48 +0700

    --fixed go back on vu history detail

--- src/pages/Details/VUHistoryDetail.jsx
+++ src/pages/Details/VUHistoryDetail.jsx
@@ -123,7 +123,7 @@ export const VUHistoryDetail = () => {
                         placement={placement}
                         name={placement}
                         bc={<FaArrowLeft />}
-                        parentLink={"/rincian-peminjaman"}
+                        parentLink={"/riwayat-peminjaman"}
                       />
                     ))}
                   </Col>
